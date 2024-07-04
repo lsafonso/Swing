ff9705a59d1c980d8a5b6ec2df8ce695461d1bf2
Leandro Afonso <leandro.sant.afonso@gmail.com>
2024-07-04 18:15:15 +0100

    Upload

---
+++ Snake/src/SnakeGame.java
@@ -0,0 +1,171 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class SnakeGame extends JPanel implements ActionListener, KeyListener {
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    int boardWidth;
+    int boardHeight;
+    int tileSize = 25;
+
+    // Snake
+    Tile snakeHead;
+    ArrayList<Tile> snakeBody;
+
+    // Food
+    Tile food;
+    Random random;
+
+    // Game logic
+    int velocityX;
+    int velocityY;
+    Timer gameLoop;
+
+    boolean gameOver = false;
+
+    SnakeGame(int boardWidth, int boardHeight) {
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
+        setBackground(Color.black);
+        addKeyListener(this);
+        setFocusable(true);
+
+        snakeHead = new Tile(5, 5);
+        snakeBody = new ArrayList<>();
+
+        food = new Tile(10, 10);
+        random = new Random();
+        placeFood();
+
+        velocityX = 1;
+        velocityY = 0;
+
+        // Game timer
+        gameLoop = new Timer(100, this); // How long it takes to start timer, milliseconds gone between frames
+        gameLoop.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        // Grid Lines
+        for (int i = 0; i < boardWidth / tileSize; i++) {
+            // (x1, y1, x2, y2)
+            g.drawLine(i * tileSize, 0, i * tileSize, boardHeight);
+            g.drawLine(0, i * tileSize, boardWidth, i * tileSize);
+        }
+
+        // Food
+        g.setColor(Color.red);
+        g.fill3DRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize, true);
+
+        // Snake Head
+        g.setColor(Color.green);
+        g.fill3DRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize, true);
+
+        // Snake Body
+        for (Tile snakePart : snakeBody) {
+            g.fill3DRect(snakePart.x * tileSize, snakePart.y * tileSize, tileSize, tileSize, true);
+        }
+
+        // Score
+        g.setFont(new Font("Arial", Font.PLAIN, 16));
+        if (gameOver) {
+            g.setColor(Color.red);
+            g.drawString("Game Over: " + snakeBody.size(), tileSize - 16, tileSize);
+        } else {
+            g.drawString("Score: " + snakeBody.size(), tileSize - 16, tileSize);
+        }
+    }
+
+    public void placeFood() {
+        food.x = random.nextInt(boardWidth / tileSize);
+        food.y = random.nextInt(boardHeight / tileSize);
+    }
+
+    public void move() {
+        // Eat food
+        if (collision(snakeHead, food)) {
+            snakeBody.add(new Tile(snakeHead.x, snakeHead.y)); // Add the head's position to the body
+            placeFood();
+        }
+
+        // Move snake body
+        for (int i = snakeBody.size() - 1; i > 0; i--) {
+            Tile prev = snakeBody.get(i - 1);
+            snakeBody.set(i, new Tile(prev.x, prev.y));
+        }
+        if (snakeBody.size() > 0) {
+            snakeBody.set(0, new Tile(snakeHead.x, snakeHead.y));
+        }
+
+        // Move snake head
+        snakeHead.x += velocityX;
+        snakeHead.y += velocityY;
+
+        // Game over conditions
+        for (Tile snakePart : snakeBody) {
+            // Collide with snake head
+            if (collision(snakeHead, snakePart)) {
+                gameOver = true;
+            }
+        }
+
+        if (snakeHead.x < 0 || snakeHead.x >= boardWidth / tileSize || // Passed left border or right border
+                snakeHead.y < 0 || snakeHead.y >= boardHeight / tileSize) { // Passed top border or bottom border
+            gameOver = true;
+        }
+    }
+
+    public boolean collision(Tile tile1, Tile tile2) {
+        return tile1.x == tile2.x && tile1.y == tile2.y;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) { // Called every x milliseconds by gameLoop timer
+        if (!gameOver) {
+            move();
+            repaint();
+        } else {
+            gameLoop.stop();
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP && velocityY != 1) {
+            velocityX = 0;
+            velocityY = -1;
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN && velocityY != -1) {
+            velocityX = 0;
+            velocityY = 1;
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT && velocityX != 1) {
+            velocityX = -1;
+            velocityY = 0;
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && velocityX != -1) {
+            velocityX = 1;
+            velocityY = 0;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+}
